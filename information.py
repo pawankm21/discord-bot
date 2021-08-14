a88b508944653e145c65fb0162eae8d58417a481
pawan <b419039@iiit-bh.ac.in>
2021-08-14 14:53:27 +0530

    changed information.py

--- information.py
+++ information.py
@@ -1,11 +1,9 @@
-BOT_KEY = ""
+
 
 JOINED_MESSAGE = "Chal jump kar"
 ON_READY = "Hello Antinormies"
-RESPONSES = [
-    
-]
-QUESTION = [
+
+QUESTIONS = [
     "It is certain.", "It is decidedly so.", "Without a doubt.",
     "Yes - definitely.", "You may rely on it.", "As I see it, yes.",
     "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
